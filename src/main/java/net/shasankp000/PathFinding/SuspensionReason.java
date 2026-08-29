dd206d5449a2818ca329bfb0bec8b08fd37df123
jaymcraft <jayden@kruyt.org>
2026-08-29 08:33:25 +0200

    Fix mining session lifecycle

--- src/main/java/net/shasankp000/PathFinding/SuspensionReason.java
+++ src/main/java/net/shasankp000/PathFinding/SuspensionReason.java
@@ -3,6 +3,7 @@ package net.shasankp000.PathFinding;
 /** Idempotent reasons that temporarily yield navigation's movement ownership. */
 public enum SuspensionReason {
     EATING,
+    MINING,
     TRADE,
     THREAT,
     COMBAT,
